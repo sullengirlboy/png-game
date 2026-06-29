a64a5e6fea161c2964bf37f87da82078d007d400
sophia <merrittsophia429@gmail.com>
2026-06-29 18:19:37 -0500

    just Some stuff

--- game.js
+++ game.js
@@ -1,7 +1,37 @@
-var list = [];
+var clothesMenuHTML = document.getElementById("clothesMenu");
 
-fetch("data.json")
+var clothesOptions = {
+    shirts: []
+};
+var listMapping = ["shirts"];
+
+populateClothes();
+
+async function populateClothes() {
+    for (var i = 0; i < listMapping.length; i++) {
+        var currentListName = listMapping[i];
+        var targetList = clothesOptions[currentListName];
+
+        await fetchData(currentListName, targetList);
+        targetList.forEach(item => {
+            var itemHTML = document.createElement("img");
+            itemHTML.src = "/img/" + currentListName + "/" + item.filePath + ".png";
+            itemHTML.addEventListener("click", function(){
+                console.log("item was clicked:", item.name);
+            })
+            clothesMenuHTML.appendChild(itemHTML);
+        })
+    }
+}
+
+async function fetchData(fileName, destinationArray) {
+    return fetch(fileName + ".json")
     .then(response => response.json())
-    .then(jsonData => jsonData.forEach(item => list.push(item)))
-
-console.log(list);
+    .then(jsonData => {
+        jsonData.forEach(singleItem => {
+            destinationArray.push(singleItem);
+        });
+        console.log(`loaded ${fileName}:`, destinationArray);
+    })
+    .catch(error => console.error(`error loading ${fileName}.json:`, error));
+}
