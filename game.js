bcfda12dbaacdafb56887153edc921750444dfa5
sophia <merrittsophia429@gmail.com>
2026-06-29 22:28:40 -0500

    layout tweaks + fade between section switch

--- game.js
+++ game.js
@@ -1,9 +1,18 @@
 var clothesMenuHTML = document.getElementById("clothesMenu");
 
+var shirtsMenuHTML = document.getElementById("shirtsMenu");
+var pantsMenuHTML = document.getElementById("pantsMenu");
+var shirtsBtn = document.getElementById("shirtsBtn");
+var pantsBtn = document.getElementById("pantsBtn");
+
 var clothesOptions = {
-    shirts: []
+    shirts: [],
+    pants: []
 };
-var listMapping = ["shirts"];
+var listMapping = ["shirts", "pants"];
+
+shirtsBtn.addEventListener("click", () => showSection("shirtsMenu"));
+pantsBtn.addEventListener("click", () => showSection("pantsMenu"));
 
 populateClothes();
 
@@ -19,7 +28,8 @@ async function populateClothes() {
             itemHTML.addEventListener("click", function(){
                 console.log("item was clicked:", item.name);
             })
-            clothesMenuHTML.appendChild(itemHTML);
+            var itemSectionHTML = document.getElementById(currentListName + "Menu")
+            itemSectionHTML.appendChild(itemHTML);
         })
     }
 }
@@ -34,4 +44,29 @@ async function fetchData(fileName, destinationArray) {
         console.log(`loaded ${fileName}:`, destinationArray);
     })
     .catch(error => console.error(`error loading ${fileName}.json:`, error));
+}
+
+function showSection(chosenSection) {
+    var element = document.getElementById(chosenSection);
+    var allSections = Array.from(clothesMenuHTML.children);
+    allSections.shift();
+
+    allSections.forEach(section => {
+        section.style.opacity = "0";
+        section.style.visibility = "hidden";
+        section.style.pointerEvents = "none";
+        
+        setTimeout(() => {
+            if (section.style.opacity === "0") {
+                section.style.display = "none";
+            }
+        }, 300);
+    });
+
+    element.style.display = 'flex';
+    setTimeout(() => {
+        element.style.opacity = "1";
+        element.style.visibility = "visible";
+        element.style.pointerEvents = "auto";
+    }, 10);
 }
